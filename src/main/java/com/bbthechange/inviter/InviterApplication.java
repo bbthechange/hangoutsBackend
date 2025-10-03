67012b5225ebb6804cc48d1c4ed4c54c394cc445
Brian Butler <bbthechange@gmail.com>
2025-10-03 03:37:43 -0700

    Tell spring we arent using userdetailsService

--- src/main/java/com/bbthechange/inviter/InviterApplication.java
+++ src/main/java/com/bbthechange/inviter/InviterApplication.java
@@ -2,10 +2,11 @@ package com.bbthechange.inviter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.event.EventListener;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class InviterApplication {
 
 	public static void main(String[] args) {
